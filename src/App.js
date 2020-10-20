d8956ee4d0461714db49917f06b652a6dde0801b
アドビ 波江野 <tos25560@adobe.com>
2020-10-20 20:38:58 +0900

    props types

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import PropTypes from 'prop-types'
 
 
 const  App = () => {
@@ -27,8 +27,9 @@ const User = (props) => {
   );
 }
 
-User.defaultProps = {
-  age: 1
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
 export default App;
