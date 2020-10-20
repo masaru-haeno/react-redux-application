9c474af6f2e871090a15405651ff6d6e34e106d1
アドビ 波江野 <tos25560@adobe.com>
2020-10-20 19:48:17 +0900

    jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 
-function App() {
-  return (
-    <h1>
-        Hello World!!.
-    </h1>
-  );
+// function App() {
+//   return (
+//     <h1>
+//         Hello World!!.
+//     </h1>
+//   );
+// }
+
+class App extends Component {
+  render() {
+
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={()=> console.log("Text Chenge")}></input>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
